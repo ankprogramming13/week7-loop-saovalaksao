a66c5e5fe7a6a87e73ca156ef1511be6b205e390
Unknown <saovalaksao@gmail.com>
2017-12-23 22:46:55 +0700

    โปรแกรมที่ 3

--- lab_7_3.cpp
+++ lab_7_3.cpp
@@ -9,8 +9,13 @@ int main() {
   // ตัวอย่างโปรแกรมที่ใช้ for
 
   int i = 0;
-  for (i = 0; i < 10; i+1) {
+  int a = 0;
+  for (i = 0; i < 10; i++) {
     cout << "Run from for" << endl;
+  }
+  while(a<10 ){
+    cout <<"Run from while" << endl;
+    a++;
   }
   cout << "************END FOR************" << endl;
 
