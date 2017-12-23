ac795cf2bdeae377fe5a773e716e90a9080b0e30
Unknown <saovalaksao@gmail.com>
2017-12-23 23:23:27 +0700

    โปรแกรมที่ 6

--- lab_7_6.cpp
+++ lab_7_6.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main () {
-
+    int number;
+    cin >> number;
+    cout << "Base\tMul\tResult" << endl;
+    for ( int a = 1; a< 13; a = a + 1 )
+    {
+        cout << number << "\t" << a <<"\t" << number*a << endl;
+    }
+    return 0;
   /* เขียนโปรแกรมตารางแม่สูตรคูณที่แสดงผลดังนี้
 
   Base    Mul   Result
@@ -18,7 +25,7 @@ int main () {
   2       9     18
   2       10    20
   2       11    22
-  2       12    24 
+  2       12    24
 
   */
 
