e6bab4e08382617d23637e67a73ea67513134b85
Unknown <saovalaksao@gmail.com>
2017-12-23 23:32:08 +0700

    โปรแกรมที่  7

--- lab_7_7.cpp
+++ lab_7_7.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main () {
+    int a = 0;
+    int num, sum = 0, avg;
+    // while loop execution
+    while( a < 5 )
+    {
+        cout << "Enter" << a + 1 << "Number:";
+        cin >> num;
+        a++;
+    }
+    avg = (float) sum / 5;
+    cout << "Total" << "\t" << avg;
 
   // เขียนโปรแกรมเพื่อรับค่าตัวเลข 5 จำนวน และหาค่าเฉลี่ย
 
