cc904f838669d14c03aa6c178af9945fa76a7c74
Unknown <unknown@example.com>
2017-12-23 22:01:28 +0700

    โปรแกรมที่ 1

--- lab_7_1.cpp
+++ lab_7_1.cpp
@@ -1,27 +1,16 @@
-#include <iostream>
+ #include <iostream>
+ using namespace std;
+ int main() {
+     cout << "This is simple loop"<<endl <<endl ;
+     int i = 0;
+     //ศึกษาตัวอย่างการใช้for
+     for(i = 0; i < 10 ; i++){
+        cout <<"Round" <<i << endl;
+     }
+     for(i=0; i <10 ; i++){
+        cout << "Hello World" <<i <<endl;
+     }
+     //TODO : แก้ไขส่วนนี้ด้วยการใช้for
 
-using namespace std;
-
-int main() {
-  
-  cout << "This is simple loop"<< endl << endl ;
-
-  int i = 0;
-
-  // ศึกษาตัวอย่างการใช้ for
-  for(i = 0; i < 10 ; i++) {
-    cout << "Round " << i << endl;
-  }
-
-  // TODO : แก้ไขส่วนนี้ด้วยการใช้ for
-  cout << "Hello World 1" << endl;
-  cout << "Hello World 2" << endl;
-  cout << "Hello World 3" << endl;
-  cout << "Hello World 4" << endl;
-  cout << "Hello World 5" << endl;
-  cout << "Hello World 6" << endl;
-  cout << "Hello World 7" << endl;
-  cout << "Hello World 8" << endl;
-
-  return 0;
-}
+     return 0;
+ }
